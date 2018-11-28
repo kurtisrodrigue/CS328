6b5163349acd8f3ba189d3076ff9cc2bd1667da9
Kurtis Rodrigue <kurtisr@nevada.unr.edu>
2018-11-27 16:03:18 -0800

    added files meant to be in last commit

---
+++ Group1/Assets/Scripts/TriggerTrap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTrap : MonoBehaviour {
+
+    private Animator anim;
+    private SpriteRenderer SR;
+    public float trap_speed;
+    public GameObject player;
+
+    void Start () 
+    {
+        anim = GetComponent<Animator>();
+        anim.speed = trap_speed;
+        SR = GetComponent<SpriteRenderer>();
+	}
+	
+	void Update () 
+    {
+		// if character position == this position
+        // might have to use a different method to check if this doesnt work
+        if(player.transform.position == transform.position)
+        {
+            // if the spikes are up
+            if(SR.sprite.name == "spike_animation_4" ||
+               SR.sprite.name == "spike_animation_5")
+            {
+                // kill the player, restart the game
+                // perhaps use invoke
+            }
+        }
+	}
+}
